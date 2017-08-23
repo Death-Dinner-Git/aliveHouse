1caee946bee3b4aafb9fd0bbc0a15e73c4df3300
Phper Helper <909387235@qq.com>
2017-08-24 00:58:55 +0800

    run service

--- public/index.php
+++ public/index.php
@@ -13,7 +13,15 @@
 
 // 定义应用目录
 define('APP_PATH', __DIR__ . '/../application/');
-//可以绑定模块,此处绑定前端
-//define('BIND_MODULE','home');
+//可以绑定模块处理
+if($_SERVER['HTTP_HOST'] == 'cms.alivehouse.com'){ //绑定三级域名 cms 到后台
+    define('BIND_MODULE','manage');
+}elseif ($_SERVER['HTTP_HOST'] == 'm.alivehouse.com'){ //绑定三级域名 m 到手机端
+    define('BIND_MODULE','phone');
+}elseif ($_SERVER['HTTP_HOST'] == 'api.alivehouse.com'){ //绑定三级域名 api 到接口
+    define('BIND_MODULE','api');
+}else{ // 其他所有的请求绑定到前端
+    define('BIND_MODULE','home');
+}
 // 加载框架引导文件
 require __DIR__ . '/../thinkphp/start.php';
